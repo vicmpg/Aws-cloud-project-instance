f3d3b7a761e51dc160eed851d49b8a9f9e1d00e5
vicmpg <vicmpg22@gmail.com>
2024-05-02 16:00:26 -0400

    3000

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
     res.end("Hello world!");
 });
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 3000; 
 
-server.listen(PORT, () => console.log("Server is running on port 5000"));
+server.listen(PORT, () => console.log("Server is running on port 3000"));
