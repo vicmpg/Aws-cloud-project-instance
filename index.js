c817728914716b7f20ed82f9306b417dd2315028
vicmpg <vicmpg22@gmail.com>
2024-05-02 16:13:29 -0400

    Adding Express

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const http = require ("http");
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello world!");
+const PORT = 3000;  // Set the port number
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000; 
-
-server.listen(PORT, () => console.log("Server is running on port 3000"));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
